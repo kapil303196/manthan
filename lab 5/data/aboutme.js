7418d108f9a8be7b264bd907efcde2a2d61cea1a
kapil upadhyay <kapil303196@gmail.com>
2020-10-07 20:21:41 +0530

    biography added

--- lab 5/data/aboutme.js
+++ lab 5/data/aboutme.js
@@ -3,7 +3,7 @@ let exportedMethods = {
     const data = {
         name: "Manthan Patel",
         cwid: "10471493",
-        biography: "asdas asda sd ad asd   \n asdasd asdasdadasdasdasd",
+        biography: "My name is Manthan Patel. I’m a 23 year old boy. I live in Ahmedabad, India. My birthday is on 3rd august. I am pursuing master degree in computer science.\n My hobbies are reading story books, playing various games , listening music and watching movies. Cricket and football are my two favourite sports. I also have a deep intrest in gadgets and technology. My dream is to work for amazon someday in the future.",
         favoriteShows: ["Peaky Blinders", "Fargo", "Breaking Bad", "Money Heist"]
     }
     return data
